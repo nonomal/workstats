b389b8c0c723d9db643c1a2e8958f6fdc2961821
Hiroshi Nishio <4620828+hiroshinishio@users.noreply.github.com>
2022-04-12 17:15:13 +0900

    Display user collection data on the user settings screen according to the logged-in user
    If there are no data to display, pre-defined placeholder text is displayed.

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -1,20 +1,27 @@
-import Head from 'next/head';
 import React from 'react';
 
-// @ts-ignore
 const InputBox = ({
   label = 'a label here',
+  inputType = 'text',
   placeholder = 'a placeholder here',
   width = 36,
+  value = undefined,
+}: {
+  label?: string;
+  inputType?: string;
+  placeholder?: string;
+  width?: number;
+  value?: string | number | undefined;
 }) => {
   return (
     <>
       <div className="px-3 py-1 mx-2 my-1">
         <span className="text-slate-800 pl-3">{label}</span>
         <input
-          type="text"
+          type={inputType}
           className={`bg-white text-gray-700 border border-gray-300 rounded-lg block w-${width} dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:placeholder-gray-400 px-2 py-1 pl-3 mt-1`}
           placeholder={placeholder}
+          value={value}
         />
       </div>
     </>
