22c261b7dbeef5ad5dcea6f6291d364b31d2f484
Khondokar Hashib <khondokar.hash@gmail.com>
2022-04-17 18:38:41 +0900

    fix: input field not taking on change event

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+interface InputBoxType {
+  label?: string;
+  inputType?: string;
+  placeholder?: string;
+  width?: number;
+  value?: string | number | undefined;
+}
 
 const InputBox = ({
   label = 'a label here',
@@ -6,13 +14,13 @@ const InputBox = ({
   placeholder = 'a placeholder here',
   width = 36,
   value = undefined,
-}: {
-  label?: string;
-  inputType?: string;
-  placeholder?: string;
-  width?: number;
-  value?: string | number | undefined;
-}) => {
+}: InputBoxType) => {
+  const [inputValue, setInputValue] = useState(value);
+
+  const handleInputOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <>
       <div className="px-3 py-1 mx-2 my-1">
@@ -21,7 +29,8 @@ const InputBox = ({
           type={inputType}
           className={`bg-white text-gray-700 border border-gray-300 rounded-lg block w-${width} dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:placeholder-gray-400 px-2 py-1 pl-3 mt-1`}
           placeholder={placeholder}
-          value={value}
+          value={inputValue}
+          onChange={(e) => handleInputOnChange(e)}
         />
       </div>
     </>
--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,5 +1,17 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./secrets.json');
+const serviceAccount = {
+    type: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_TYPE,
+    project_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID,
+    private_key_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY_ID,
+    private_key: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY,
+    client_email: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
+    client_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_ID,
+    auth_uri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_AUTH_URI,
+    token_uri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_X509_CERT_URL
+};
+
 // @ts-ignore
 const verifyIdToken = (token) => {
     if (!admin.apps.length) {
