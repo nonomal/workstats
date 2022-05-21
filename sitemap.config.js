4f9ad659b2a8fe48c460bd20af75b443fa881aa7
Hiroshi Nishio <4620828+hiroshinishio@users.noreply.github.com>
2022-05-21 20:45:17 +0900

    Resolved mis-configuration for sitemap location (#57)

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteUrl: 'https://workstats.dev/',
   generateRobotsTxt: true, // Generate robots.txt, default is false
-  sitemapSize: 7000, // Split sitemap into multiple files, default is 5000
-  outDir: './out'
+  sitemapSize: 7000 // Split sitemap into multiple files, default is 5000
+  // outDir: './out' // If we use both SSG and SSR, we can't use `next export` and the directory of `./out`
 };
