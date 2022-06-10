5744d185714e5a51c2284271d794694d3f3327fb
Hiroshi Nishio <4620828+hiroshinishio@users.noreply.github.com>
2022-06-11 00:53:43 +0900

    Reduced size of environment variables which is not secrets

--- config/firebaseClient.js
+++ config/firebaseClient.js
@@ -5,9 +5,9 @@ import { getAuth, GithubAuthProvider, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  authDomain: 'polygonhr-d5153.firebaseapp.com',
+  projectId: 'polygonhr-d5153',
+  storageBucket: 'polygonhr-d5153.appspot.com',
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
